0ab4e1583c3e97dcfc838d1a4771fa6bade94266
Joshua Madrigal <madrigaljosh10@yahoo.com>
2018-11-29 16:53:30 -0600

    Get the images inside of the PokemonCards folder Add tracking images to configuration and set max number of tracked images. Know when an anchor is added to the scene and check to see if that is an ARImage anchor that can be used. How to create and add a plane and rotate it along the x axis.

---
+++ CardImageTracking/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  CardImageTracking
+//
+//  Created by Joshua Madrigal on 11/29/18.
+//  Copyright © 2018 joshuamadrigal. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "PokemonCards", bundle: Bundle.main) {
+            configuration.trackingImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 3
+        }
+        
+        
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let size = imageAnchor.referenceImage.physicalSize
+            let plane = SCNPlane(width: size.width, height: size.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            plane.cornerRadius = 0.005
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi/2
+            
+            node.addChildNode(planeNode)
+            
+        }
+        
+        return node
+        
+    }
+
+ 
+} // end ViewController
